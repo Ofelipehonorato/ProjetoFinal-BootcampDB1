44a04aa756dad9f6a70660afdab547c1a9278da2
Ofelipehonorato <fefranciscato@gmail.com>
2023-07-21 23:33:38 -0300

    configuração para rotas de autenticação

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const sequelize = require('../database/conection');
 
-/* GET home page. */
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('testes');
+});
+
+router.get('/healthcheck', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.status(204).send();
+  } catch (error) {
+    console.warn(error);
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
